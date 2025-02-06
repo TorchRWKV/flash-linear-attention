decc9035116296d714112b3039634181dd47e21a
zhiyuan li <uniartisan2017@gmail.com>
2025-02-06 08:27:27 +0800

    f

--- fla/ops/rwkv7/__init__.py
+++ fla/ops/rwkv7/__init__.py
@@ -2,8 +2,12 @@
 
 from .chunk import chunk_rwkv7
 from .fused_recurrent import fused_recurrent_rwkv7
+from .recurrent_naive import native_recurrent_rwkv7
+from .channel_mixing import channel_mixing_rwkv7
 
 __all__ = [
     'chunk_rwkv7',
-    'fused_recurrent_rwkv7'
+    'fused_recurrent_rwkv7',
+    'native_recurrent_rwkv7',
+    'channel_mixing_rwkv7'
 ]

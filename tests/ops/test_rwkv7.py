3dd26b5d2304a98dfe2b3a72ad7ed6272fbbd791
zhiyuan li <uniartisan2017@gmail.com>
2025-02-07 15:01:02 +0800

    add fused_addcmul_rwkv7

--- tests/ops/test_rwkv7.py
+++ tests/ops/test_rwkv7.py
@@ -19,6 +19,8 @@ from fla.ops.rwkv7.channel_mixing import (
     channel_mixing_rwkv7,
 )
 from fla.ops.rwkv7.fused_recurrent import fused_recurrent_rwkv7
+from fla.ops.rwkv7.fused_addcmul import fused_addcmul_rwkv7, torch_addcmul_rwkv7
+import torch.test
 from utils import assert_close
 torch.backends.cudnn.allow_tf32 = False
 torch.backends.cuda.matmul.allow_tf32 = False
@@ -231,3 +233,89 @@ def test_fused_recurrent_rwkv7(
         print("Forward pass test passed", (ref_o - result).abs().max().item())
         assert get_err_ratio(ref_o, result) < 5e-4
     print('test passed')
+
+
+@pytest.mark.parametrize("B", [4])
+@pytest.mark.parametrize("T", [4096])
+@pytest.mark.parametrize("H", [64])
+@pytest.mark.parametrize("D", [64])
+@pytest.mark.parametrize("dtype", [torch.float32])
+@pytest.mark.parametrize("use_g", [True, False])
+def test_fused_rwkv7_addcmul(
+    B: int,
+    T: int,
+    H: int,
+    D: int,
+    dtype: torch.dtype,
+    use_g: bool
+):
+    T = 4096
+    B = 4
+    hidden_size = H*D
+    hidden_states = torch.randn(B, T, hidden_size).uniform_(-8,8).to(device).to(dtype).requires_grad_()
+    xx = torch.randn(B, T, hidden_size).uniform_(-8,8).to(device).to(dtype).requires_grad_()
+    x_r = torch.randn(1, 1, hidden_size).uniform_(-8,8).to(device).to(dtype).requires_grad_()
+    x_w = torch.randn(1, 1, hidden_size).uniform_(-8,8).to(device).to(dtype).requires_grad_()
+    x_k = torch.randn(1, 1, hidden_size).uniform_(-8,8).to(device).to(dtype).requires_grad_()
+    x_v = torch.randn(1, 1, hidden_size).uniform_(-8,8).to(device).to(dtype).requires_grad_()
+    x_a = torch.randn(1, 1, hidden_size).uniform_(-8,8).to(device).to(dtype).requires_grad_()
+    if use_g:
+        x_g = torch.randn(1, 1, hidden_size).uniform_(-8,8).to(device).to(dtype).requires_grad_()
+    else:
+        x_g = None
+    xr0, xw0, xk0, xv0, xa0, xg0 = fused_addcmul_rwkv7(hidden_states, xx, x_r, x_w, x_k, x_v, x_a, x_g)
+    xr1, xw1, xk1, xv1, xa1, xg1 = torch_addcmul_rwkv7(hidden_states, xx, x_r, x_w, x_k, x_v, x_a, x_g)
+    torch.testing.assert_close(xr0, xr1, rtol=1e-3, atol=1e-3)
+    torch.testing.assert_close(xw0, xw1, rtol=1e-3, atol=1e-3)
+    torch.testing.assert_close(xk0, xk1, rtol=1e-3, atol=1e-3)
+    torch.testing.assert_close(xv0, xv1, rtol=1e-3, atol=1e-3)
+    torch.testing.assert_close(xa0, xa1, rtol=1e-3, atol=1e-3)
+    if use_g:
+        torch.testing.assert_close(xg0, xg1, rtol=1e-3, atol=1e-3)
+        (xr0 + xw0 + xk0 + xv0 + xa0 + xg0).sum().backward()
+    else:
+        (xr0 + xw0 + xk0 + xv0 + xa0).sum().backward()
+    d_ixr = x_r.grad.clone()
+    d_ixw = x_w.grad.clone()
+    d_ixk = x_k.grad.clone()
+    d_ixv = x_v.grad.clone()
+    d_ixa = x_a.grad.clone()
+    d_hidden = hidden_states.grad.clone()
+    d_xx = xx.grad.clone()
+
+    x_r.grad.zero_()
+    x_w.grad.zero_()
+    x_k.grad.zero_()
+    x_v.grad.zero_()
+    x_a.grad.zero_()
+    if use_g:
+        d_ixg = x_g.grad.clone()
+        x_g.grad.zero_()
+    hidden_states.grad.zero_()
+    xx.grad.zero_()
+
+    if use_g:
+        (xr1 + xw1 + xk1 + xv1 + xa1 + xg1).sum().backward()
+    else:
+        (xr1 + xw1 + xk1 + xv1 + xa1).sum().backward()
+    d_ixr1 = x_r.grad.clone()
+    d_ixw1 = x_w.grad.clone()
+    d_ixk1 = x_k.grad.clone()
+    d_ixv1 = x_v.grad.clone()
+    d_ixa1 = x_a.grad.clone()
+    if use_g:
+        d_ixg1 = x_g.grad.clone()
+    d_hidden1 = hidden_states.grad.clone()
+    d_xx1 = xx.grad.clone()
+
+    torch.testing.assert_close(d_ixr, d_ixr1, rtol=1e-3, atol=1e-3)
+    torch.testing.assert_close(d_ixw, d_ixw1, rtol=1e-3, atol=1e-3)
+    torch.testing.assert_close(d_ixk, d_ixk1, rtol=1e-3, atol=1e-3)
+    torch.testing.assert_close(d_ixv, d_ixv1, rtol=1e-3, atol=1e-3)
+    torch.testing.assert_close(d_ixa, d_ixa1, rtol=1e-3, atol=1e-3)
+    if use_g:
+        torch.testing.assert_close(d_ixg, d_ixg1, rtol=1e-3, atol=1e-3)
+    torch.testing.assert_close(d_hidden, d_hidden1, rtol=1e-3, atol=1e-3)
+    torch.testing.assert_close(d_xx, d_xx1, rtol=1e-3, atol=1e-3)
+
+
